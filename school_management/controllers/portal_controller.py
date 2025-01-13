6d0a875dd08a035179628013796109f45ff0fb37
shamim.hossen <shamim.hossen@bjitgroup.com>
2025-01-14 05:11:03 +0600

    see couter variables

--- school_management/controllers/portal_controller.py
+++ school_management/controllers/portal_controller.py
@@ -1,8 +1,10 @@
 from odoo.addons.portal.controllers.portal import CustomerPortal
-
+from odoo.http import request
 
 class MySchoolPortal(CustomerPortal):
     def _prepare_home_portal_values(self, counters):
         values = super(MySchoolPortal, self)._prepare_home_portal_values(counters)
+        # values['school_count'] = request.env['school_management.school'].search_count([])
+        values['school_count'] = 10
         print("values: ", values)
         return values
