148485a0b64b8d2e161db43e50494d1326984089
Shamim Razu <shamimhossenrazu@gmail.com>
2025-01-21 12:10:19 +0600

    access context variable active_id, active_ids, active_model

--- school_management/wizard/student_data_update_wizard.py
+++ school_management/wizard/student_data_update_wizard.py
@@ -9,5 +9,10 @@ class StudentDataUpdate(models.TransientModel):
     weight_in_pounds = fields.Float()
 
     def update_data(self):
+        print('Context_data', self.env.context.get('active_ids'))
+        print('Context_data', self._context['active_ids'])
+        print('Active id', self.env.context.get('active_id'))
+        print('Active id', self._context['active_id'])
+        print('Active model', self.env.context.get('active_model'))
         pass
 
